3531eb3b216638328ee9128c89a1942900842ba7
Aygun77 <101467805+Aygun77@users.noreply.github.com>
2022-03-16 12:13:26 -0500

    added comment

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main {
   public static void main(String[] args) {
     //tests
+	  //sfsndcs
     System.out.println(generatePlate("VA")); // -> e.g "ADS-2252"
     System.out.println(generatePlate("MD")); // -> e.g "3BS6121"
     System.out.println(generatePlate("DC")); // -> e.g "NS7821"
